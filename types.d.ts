b953cec65df74d03951d56b7cc16d4684f9a764f
Ariet Nasirdinov <ariet.nasirdinov505@gmail.com>
2024-04-22 15:40:56 +0600

    Create user and task interfaces, configure ts and nodemon

---
+++ types.d.ts
@@ -0,0 +1,12 @@
+export interface UserFields {
+  username: string;
+  password: string;
+  token: string;
+}
+
+export interface TaskFields {
+  user: string;
+  title: string;
+  description: string | null;
+  status: string;
+}
